7d1cf707559d43b5799a6c6928734499d919887c
gauravborra <gaurav.kage@gmail.com>
2020-05-13 02:17:12 +0530

    pages added

--- JS/mousemoveparallax.js
+++ JS/mousemoveparallax.js
@@ -1,4 +1,4 @@
-(function() {
+/*(function() {
     // Add event listener
     document.addEventListener("mousemove", parallax);
     const elem = document.querySelector("#parallax");
@@ -17,3 +17,36 @@
     }
 
 })();
+
+*/
+
+
+const cards = document.querySelector(".parallax_card");
+const images = document.querySelectorAll(".parallax_fore");
+const backgrounds = document.querySelectorAll(".parallax_bg");
+const range = 40;
+
+// const calcValue = (a, b) => (((a * 100) / b) * (range / 100) -(range / 2)).toFixed(1);
+const calcValue = (a, b) => (a/b*range-range/2).toFixed(1) // thanks @alice-mx
+
+let timeout;
+document.addEventListener('mousemove', ({x, y}) => {
+  if (timeout) {
+    window.cancelAnimationFrame(timeout);
+  }
+  	
+  timeout = window.requestAnimationFrame(() => {
+    const yValue = calcValue(y, window.innerHeight);
+    const xValue = calcValue(x, window.innerWidth);
+
+    cards.style.transform = `rotateX(${yValue}deg) rotateY(${xValue}deg)`;
+
+    [].forEach.call(images, (image) => {
+      image.style.transform = `translateX(${-xValue}px) translateY(${yValue}px)`;
+    });
+
+    [].forEach.call(backgrounds, (background) => {
+      background.style.backgroundPosition = `${xValue*.45}px ${-yValue*.45}px`;
+    })
+	})
+}, false);
